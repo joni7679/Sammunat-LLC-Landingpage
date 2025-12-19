a078bd6a43203b4193f16736b2a500d09e569991
joni <codingmylife0@gmail.com>
2025-12-20 00:28:08 +0530

    responsive navbar fixed

---
+++ src/components/Loader.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <>
+    <div id="loader">
+  <div class="dot" data-depth="0.20"><span>L</span></div>
+  <div class="dot" data-depth="0.20"><span>O</span></div>
+  <div class="dot" data-depth="0.20"><span>A</span></div>
+  <div class="dot" data-depth="0.20"><span>D</span></div>
+  <div class="dot" data-depth="0.20"><span>I</span></div>
+  <div class="dot" data-depth="0.20"><span>N</span></div>
+  <div class="dot" data-depth="0.20"><span>G</span></div>
+</div>
+    </>
+  )
+}
+
+export default Loader
